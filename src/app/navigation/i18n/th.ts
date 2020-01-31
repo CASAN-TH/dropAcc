2a94f58ffac459dce840cfec0e96554139e64d2b
Theerasak Tubrit <theerasaktubrit@Theerasaks-MacBook-Pro.local>
2020-01-31 20:18:44 +0700

    initial web dropAcc

---
+++ src/app/navigation/i18n/th.ts
@@ -0,0 +1,12 @@
+export const locale = {
+    lang: 'th',
+    data: {
+        'NAV': {
+            'APPLICATIONS': 'ฟังชั่นงาน',
+            'SAMPLE'        : {
+                'TITLE': 'ตัวอย่าง',
+                'BADGE': '25'
+            }
+        }
+    }
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    hmr       : false,
+    appName : 'dropAcc-prod',
+    apiUrl : 'http://'
+};
